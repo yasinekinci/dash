17edf1d1f5bb47ef2a07fa2e5d41c794d3e9e47a
yasinekinci <yasinekkinci@gmail.com>
2022-01-16 00:27:19 +0300

    add user configuration

---
+++ src/backend/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,21 @@
+
+using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).UseIdentityColumn();
+        builder.Property(x => x.FirstName).IsRequired().HasMaxLength(50);
+        builder.Property(x => x.LastName).IsRequired().HasMaxLength(50);
+        builder.Property(x => x.Email).IsRequired();
+
+        builder.ToTable("Users");
+    }
+}
