e678d8e1d582bc1946cb43da6609dd38fa815c2e
yasinekinci <yasinekkinci@gmail.com>
2022-01-16 23:01:55 +0300

    add user model

---
+++ src/backend/Domain/Models/User/UserModel.cs
@@ -0,0 +1,12 @@
+namespace Domain
+{
+    public class UserModel : BaseModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PasswordSalt { get; set; }
+        public string PasswordHash { get; set; }
+        public bool Status { get; set; }
+    }
+}
