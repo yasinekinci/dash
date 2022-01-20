e73e1ea7a7bdb13f4defb72abb49701172ba7b10
yasinekinci <yasinekkinci@gmail.com>
2022-01-20 14:07:39 +0300

    add httpStatusCode

--- src/backend/Domain/Models/Response/ResponseModel.cs
+++ src/backend/Domain/Models/Response/ResponseModel.cs
@@ -1,29 +1,30 @@
+using System.Net;
 using System.Text.Json.Serialization;
 
 namespace Domain.Models.Response
 {
-    public class ResponseModel<T> where T : BaseModel
+    public class ResponseModel<T> where T : class
     {
         [JsonIgnore]
-        public int StatusCode { get; set; }
-        public T Result { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public T Data { get; set; }
         public List<string> Errors { get; set; }
-
-        public static ResponseModel<T> Success(int statusCode, T result)
+        public bool Result { get; set; }
+        public static ResponseModel<T> Success(T data, HttpStatusCode statusCode = HttpStatusCode.OK)
         {
-            return new ResponseModel<T> { Result = result, StatusCode = statusCode };
+            return new ResponseModel<T> { Data = data, StatusCode = statusCode, Result = true };
         }
-        public static ResponseModel<T> Success(int statusCode)
+        public static ResponseModel<T> Success(HttpStatusCode statusCode = HttpStatusCode.OK)
         {
-            return new ResponseModel<T> { StatusCode = statusCode };
+            return new ResponseModel<T> { StatusCode = statusCode, Result = true };
         }
-        public static ResponseModel<T> Fail(int statusCode, List<string> errors)
+        public static ResponseModel<T> Fail(List<string> errors, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
         {
-            return new ResponseModel<T> { StatusCode = statusCode, Errors = errors };
+            return new ResponseModel<T> { StatusCode = statusCode, Errors = errors, Result = false };
         }
-        public static ResponseModel<T> Fail(int statusCode, string error)
+        public static ResponseModel<T> Fail(string error, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
         {
-            return new ResponseModel<T> { StatusCode = statusCode, Errors = new List<string>() { error } };
+            return new ResponseModel<T> { StatusCode = statusCode, Errors = new List<string>() { error }, Result = false };
         }
 
     }
