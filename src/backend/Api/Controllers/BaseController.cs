546845b10e5c99c65934cdfb372fea0d14b589e1
yasinekinci <yasinekkinci@gmail.com>
2022-01-20 14:08:28 +0300

    add common CreateActionResult

--- src/backend/Api/Controllers/BaseController.cs
+++ src/backend/Api/Controllers/BaseController.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using Domain;
+using Domain.Models.Response;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
@@ -6,5 +9,16 @@ namespace Api.Controllers;
 [Route("[controller]")]
 public class BaseController : ControllerBase
 {
+    public IActionResult CreateActionResult<T>(T result, HttpStatusCode statusCode = HttpStatusCode.OK) where T : class
+    {
+        switch (statusCode)
+        {
+            case HttpStatusCode.BadRequest:
+                return BadRequest(ResponseModel<T>.Success(result, statusCode));
+
+            default:
+                return Ok(ResponseModel<T>.Success(result, statusCode));
+        }
+    }
 
 }
