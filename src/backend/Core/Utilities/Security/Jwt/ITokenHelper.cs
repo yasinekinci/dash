dbb1711e89d2320fe841191f07f2a208bcd16cf6
yasinekinci <yasinekkinci@gmail.com>
2022-01-21 21:54:38 +0300

    add jwt token operations

--- src/backend/Core/Utilities/Security/Jwt/ITokenHelper.cs
+++ src/backend/Core/Utilities/Security/Jwt/ITokenHelper.cs
@@ -4,6 +4,6 @@ namespace Core.Utilities.Security.Jwt
 {
     public interface ITokenHelper
     {
-        AccessToken CreateToken(User user);
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
     }
 }
