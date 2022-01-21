2a201e2502900d6770a975222f156d2028b9c188
yasinekinci <yasinekkinci@gmail.com>
2022-01-21 17:21:34 +0300

    change proprerty type of password salt and password hash

--- src/backend/Domain/Models/User/UserModel.cs
+++ src/backend/Domain/Models/User/UserModel.cs
@@ -7,8 +7,8 @@ namespace Domain
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string PasswordSalt { get; set; }
-        public string PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
         public bool Status { get; set; }
 
         public ICollection<UserOperationClaimModel> UserOperationClaim { get; set; }
