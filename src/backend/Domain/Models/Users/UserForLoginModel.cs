36bf8979049287b412d8a5a61c2d49a604a3db8e
yasinekinci <yasinekkinci@gmail.com>
2022-01-22 13:27:22 +0300

    add login and register model

---
+++ src/backend/Domain/Models/Users/UserForLoginModel.cs
@@ -0,0 +1,8 @@
+namespace Domain.Models.Users
+{
+    public class UserForLoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/backend/Domain/Models/Users/UserForRegisterModel.cs
@@ -0,0 +1,10 @@
+namespace Domain.Models.Users
+{
+    public class UserForRegisterModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
