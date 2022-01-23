27f5a107448df718fa0987c8df024c41e08a7bc3
yasinekinci <yasinekkinci@gmail.com>
2022-01-23 13:54:52 +0300

    remove CreateActionResult and response model

--- src/backend/Api/Controllers/BaseController.cs
+++ src/backend/Api/Controllers/BaseController.cs
@@ -1,6 +1,3 @@
-using System.Net;
-using Domain;
-using Domain.Models.Response;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
@@ -9,19 +6,5 @@ namespace Api.Controllers;
 [Route("[controller]")]
 public class BaseController : ControllerBase
 {
-    public IActionResult CreateActionResult<T>(T result, HttpStatusCode statusCode = HttpStatusCode.OK) where T : class
-    {
-        switch (statusCode)
-        {
-            case HttpStatusCode.BadRequest:
-                return BadRequest(ResponseModel<T>.Success(result, statusCode));
-            case HttpStatusCode.Created:
-                return Created(string.Empty, ResponseModel<T>.Success(result, statusCode));
-            case HttpStatusCode.NoContent:
-                return Ok(ResponseModel<T>.Success());
-            default:
-                return Ok(ResponseModel<T>.Success(result, statusCode));
-        }
-    }
-
+  
 }
