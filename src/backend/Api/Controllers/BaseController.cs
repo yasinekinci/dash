9b91da42296b4437fdaff2d850d3ffd0a694ddd4
yasinekinci <yasinekkinci@gmail.com>
2022-01-24 10:16:41 +0300

    add create action data result

--- src/backend/Api/Controllers/BaseController.cs
+++ src/backend/Api/Controllers/BaseController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+using Core.Utilities.Results;
 namespace Api.Controllers;
 
 [Authorize]
@@ -8,5 +8,33 @@ namespace Api.Controllers;
 [Route("api/[controller]")]
 public class BaseController : ControllerBase
 {
-  
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public IDataResult<T> CreateActionDataResult<T>(T result, string message, bool success = true) where T : class
+    {
+        if (success)
+        {
+            return new SuccessDataResult<T>(result, message);
+        }
+        return new ErrorDataResult<T>(result, message);
+    }
+
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public IDataResult<T> CreateActionDataResult<T>(T result, bool success = true) where T : class
+    {
+        if (success)
+        {
+            return new SuccessDataResult<T>(result);
+        }
+        return new ErrorDataResult<T>(result);
+    }
+
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public Core.Utilities.Results.IResult CreateActionResult(bool success = true)
+    {
+        if (success)
+        {
+            return new SuccessResult();
+        }
+        return new ErrorResult();
+    }
 }
