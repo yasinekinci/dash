ceed59208b77befa7e2b1fd466939c2f5092eadb
yasinekinci <yasinekkinci@gmail.com>
2022-01-25 22:16:25 +0300

    add  method for adding products.

--- src/backend/Api/Controllers/ProductsController.cs
+++ src/backend/Api/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
 
+using AutoMapper;
+using Domain.Entites;
+using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using Service;
 
@@ -6,16 +9,27 @@ namespace Api.Controllers
 {
     public class ProductsController : BaseController
     {
+        private readonly IMapper _mapper;
         private readonly IProductService _productService;
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, IMapper mapper)
         {
             _productService = productService;
+            _mapper = mapper;
         }
 
         [HttpPost]
         public async Task<IActionResult> GetAllProduct()
         {
             return Ok(await _productService.GetAllAsync());
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add(ProductModel productModel)
+        {
+            var entity = _mapper.Map<Product>(productModel);
+            var result = await _productService.InsertAsync(entity);
+            var model = _mapper.Map<ProductModel>(result);
+            return Created(string.Empty, CreateActionDataResult(model, "Product added."));
         }
 
     }
