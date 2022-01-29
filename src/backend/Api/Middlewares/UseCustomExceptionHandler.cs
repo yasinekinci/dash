e4096a91af1380123bff37196dd20ea22438fb2c
yasinekinci <yasinekkinci@gmail.com>
2022-01-29 13:57:53 +0300

    add not found exception

--- src/backend/Api/Middlewares/UseCustomExceptionHandler.cs
+++ src/backend/Api/Middlewares/UseCustomExceptionHandler.cs
@@ -19,6 +19,7 @@ namespace Api.Middlewares
                     var statusCode = exceptionFeature.Error switch
                     {
                         ClientSideException => 400,
+                        NotFoundException => 404,
                         _ => 500
                     };
                     context.Response.StatusCode = statusCode;
---
+++ src/backend/Core/Extensions/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+namespace Core.Extensions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
