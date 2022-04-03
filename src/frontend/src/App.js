89cebee767c75c417391b28a6f702a0e5c133ac2
yasinekinci <yasinekinci@yandex.com>
2022-04-03 12:14:13 +0300

    add mui icons

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,14 +1,7 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import { Checkbox } from '@mui/material';
-
 function App() {
   return (
-    <div >
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <Checkbox/>
+    <div>
+      Dashboard
     </div>
   );
 }
--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>  <App /></React.StrictMode>);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
